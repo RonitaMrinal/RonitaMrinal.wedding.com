978ea96afd652cd78aca5c98718d4e976c94d594
RonitaMrinal <154190744+RonitaMrinal@users.noreply.github.com>
2023-12-19 23:27:20 -0800

    update excel

--- js/scripts.js
+++ js/scripts.js
@@ -218,7 +218,7 @@ $(document).ready(function () {
             && MD5($('#invite_code').val()) !== '2ac7f43695eb0479d5846bb38eec59cc') {
             $('#alert-wrapper').html(alert_markup('danger', '<strong>Sorry!</strong> Your invite code is incorrect.'));
         } else {
-            $.post('https://script.google.com/macros/s/AKfycbzhqYG6WPZNtjJpsP-_raZv9vtql7KoVQZVvVhH6PXXoGVBvl1_bP1xnuH7n6Ud-AO0/exec', data)
+            $.post('https://script.google.com/macros/s/AKfycbzMBrt2B2WY-6AxEyfASwQp_uOW9TvE7BsrbaVX_L7_NTqMYrMTt9_azw5H6WwIKkKJ/exec', data)
             
                 .done(function (data) {
                     console.log(data);
