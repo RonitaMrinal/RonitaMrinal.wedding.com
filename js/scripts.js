135beec68ed033a9e8cf2f1f020ac22ba695c68a
RonitaMrinal <154190744+RonitaMrinal@users.noreply.github.com>
2023-12-20 21:21:44 -0800

    update calendar

--- js/scripts.js
+++ js/scripts.js
@@ -206,6 +206,37 @@ $(document).ready(function () {
 
     $('#add-to-cal').html(myCalendar);
 
+    /********************** Add to Calendar **********************/
+    var myCalendar = createCalendarUpdate({
+        options: {
+            class: '',
+            // You can pass an ID. If you don't, one will be generated for you
+            id: ''
+        },
+        data: {
+            // Event title
+            title: "Mrinal and Ronita's Wedding",
+
+            // Event start date
+            start: new Date('Feb 11, 2024 10:00'),
+
+            // Event duration (IN MINUTES)
+            // duration: 120,
+
+            // You can also choose to set an end time
+            // If an end time is set, this will take precedence over duration
+            end: new Date('Feb 12, 2024 00:00'),
+
+            // Event Address
+            address: 'Grand Uniara, Jaipur',
+
+            // Event Description
+            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Vibhor Mathur at +91 919610194878 and Mr. Rohit Mitra at +91 9722483079"
+        }
+    });
+
+    $('#add-to-calupdate').html(createCalendarUpdate);
+
 
     /********************** RSVP **********************/
     $('#rsvp-form').on('submit', function (e) {
